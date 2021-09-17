f8146fd8c143fbbef9c072bf4c5939bf75cbb4f7
Pedro Luiz <pedro.cardenuto@hotmail.com>
2021-09-16 22:47:25 -0300

    final da aula

--- gravaDisciplina.php
+++ gravaDisciplina.php
@@ -1,12 +1,15 @@
 <?php
-$_POST['disciplina'] = $_POST['disciplina'] ?? 'Nao Informado';
-$_POST['professor'] = $_POST['professor'] ?? 'Nao Informado';
-$_POST['dia'] = $_POST['dia'] ?? 'Nao Informado';
-$_POST['descricao'] = $_POST['descricao'] ?? 'Nao Informado';
+$_POST['nome'] = $_POST['nome'] ?? '';
+$_POST['professor'] = $_POST['professor'] ?? '';
+$_POST['dia'] = $_POST['dia'] ?? '';
+$_POST['descricao'] = $_POST['descricao'] ?? '';
 
-echo "Nome da disciplina: ". $_POST['disciplina'] . "<br>". "Nome do professor: ". $_POST['professor']. "<br>".
+echo "Nome da disciplina: ". $_POST['nome'] . "<br>". "Nome do professor: ". $_POST['professor']. "<br>".
 "Dia da discplina: ". $_POST['dia'] . "<br>" . "Descrição: ". $_POST['descricao'];
 
+if(empty($_POST['nome']) || empty($_POST['dia']) ) {
+    die('<br><br>ERRO! Os campos nome e dia sao obrigatorios');
+}
 //abro o arquivo para gravar mais coisas nele
 $arquivo = fopen('bancodedados.csv', 'a');
 
@@ -20,9 +23,9 @@ $timestamp = strtotime($dia);
 $dia = date( 'd/m/Y', $timestamp);
 
 //escrevo no arquivo
-fwrite($arquivo, "{$_POST['disciplina']};{$_POST['professor']}; $dia ;{$_POST['descricao']}\r\n");
+fwrite($arquivo, "{$_POST['nome']};{$_POST['professor']}; $dia ;{$_POST['descricao']}\r\n");
 
 //fecho o arquivo
 fclose($arquivo);
 
-echo "<br><br>{$_POST['disciplina']} gravada com sucesso!";
+echo "<br><br>{$_POST['nome']} gravada com sucesso!";
---
+++ gravaDisciplinaBD.php
@@ -0,0 +1,46 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$_POST['nome'] = $_POST['nome'] ?? '';
+$_POST['professor'] = $_POST['professor'] ?? '';
+$_POST['dia'] = $_POST['dia'] ?? '';
+$_POST['descricao'] = $_POST['descricao'] ?? '';
+
+if(empty($_POST['nome']) || empty($_POST['dia']) ) {
+    die('<br><br>ERRO! Os campos nome e dia sao obrigatorios');
+}
+
+echo "Nome da disciplina: ". $_POST['nome'] 
+. "<br>". "Nome do professor: ". $_POST['professor'] 
+. "<br>"."Dia da discplina: ". $_POST['dia'] 
+. "<br>" . "Descrição: ". $_POST['descricao'];
+
+$bd_dsn = 'mysql:host=localhost;port=3306;dbname=ling_serv';
+$bd_user = 'root';
+$bd_pass = '';
+
+//Conectamos com o Banco MySQL
+$bd = new PDO($bd_dsn, $bd_user, $bd_pass);
+
+//Preparamos a consulta para evitar SQL Injection
+$stmt = $bd->prepare('INSERT disciplinas 
+                            (nome, professor, dia, descricao, end_ip) 
+                        VALUES
+                            (:nome, :professor, :dia, :descricao, :end_ip)');
+
+//Colocamos os valores para serem inseridos no banco
+//em um vetor linkando label com valor passado pelo usuario
+$valores[':nome'] = $_POST['nome'];
+$valores[':professor'] = $_POST['professor'];
+$valores[':dia'] = $_POST['dia'];
+$valores[':descricao'] = $_POST['descricao'];
+$valores[':end_ip'] = $_SERVER['REMOTE_ADDR'];
+
+//Executamos a consulta SQL
+if($stmt->execute($valores)){
+    echo "<br><br>dados inseridos com sucesso";
+} else {
+    echo "<br><br> Erro, Nao consegui gravar no banco";
+}
