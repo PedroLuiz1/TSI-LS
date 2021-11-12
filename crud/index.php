79c693536136f263392d0b9e784e18de1d9d2a76
Pedro Luiz <pedro.cardenuto@hotmail.com>
2021-11-11 22:46:40 -0300

    final da aula

---
+++ crud/index.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'banco/pdo.php';
+
+include 'classes/Disciplina.class.php';
+
+$objDisciplina = new Disciplina($bd);
+
+include 'telas/header.php';
+
+if(isset($_POST['apagar'])) {
+
+   $apagado = $objDisciplina->apagar($_POST['apagar']);
+}
+
+if(isset($_POST['criar'])){
+
+    $criado = $objDisciplina->criar();
+}
+
+$lista = $objDisciplina->listar();
+
+include 'telas/lista.php';
+
+include 'telas/footer.php';
+
---
+++ crud/telas/cadastro.php
@@ -0,0 +1,20 @@
+<form action="index.php" method="post">
+    <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" id="nome" name="nome">
+    </div>
+
+    <div class="form-group">
+        <label for="prof">Professor</label>
+        <input type="text" class="form-control" id="prof" name="prof">
+    </div>
+
+    <div class="form-group">
+        <label for="dia">Dia</label>
+        <input type="date" class="form-control" id="dia" name="dia">
+    </div>
+
+    <div class="form-group">
+        <label for="desc">Descriçao</label>
+        <textarea name="desc" class="form-control" id="desc" name="desc"></textarea>
+    </div>
